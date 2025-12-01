495f57ca5810584f3cc416977aefc2231e86c7af
MarioJeconiah <mariopurba40@gmail.com>
2025-12-01 15:22:50 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,17 @@
-const { pool } = require("pg");
-const dotenv = require("dotenv");
+const { Pool } = require("pg");
 
-// Aktifkan dotenv untuk memuat variabel lingkungan dari file .env
+
+const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+const pool = new Pool ({
+    user: process.env.PGUSER,
+    host : process.env.PGHOST,
+    database : process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
 
-  ssl: {
-    rejectUnauthorized: false,
-  },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-
-module.exports = pool ;
+module.exports = pool;
